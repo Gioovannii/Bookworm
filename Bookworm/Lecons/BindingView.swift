5aa0c2e4dead28d01c0524a52aa272bc206b1d2a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-08 10:16:16 +0800

    Update project

---
+++ Bookworm/Lecons/BindingView.swift
@@ -0,0 +1,43 @@
+//
+//  BindingView.swift
+//  Bookworm
+//
+//  Created by Giovanni Gaffé on 2021/11/8.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColor = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColor), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct BindingView: View {
+    @State private var rememberMe = false
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct BindingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingView()
+    }
+}
