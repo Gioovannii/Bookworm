d66e3654cccaa22ae0252b8bf4a0808094a0349b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-08 10:17:45 +0800

    New file type erasure

---
+++ Bookworm/Lecons/TypeErasureView.swift
@@ -0,0 +1,20 @@
+//
+//  TypeErasureView.swift
+//  Bookworm
+//
+//  Created by Giovanni Gaffé on 2021/11/8.
+//
+
+import SwiftUI
+
+struct TypeErasureView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TypeErasureView_Previews: PreviewProvider {
+    static var previews: some View {
+        TypeErasureView()
+    }
+}
