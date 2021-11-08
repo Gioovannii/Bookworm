4d819c0bc5a76cdfdd7d54decbf157ed1ae2963b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-08 13:48:40 +0800

    set some different stack in different device

--- Bookworm/Lecons/TypeErasureView.swift
+++ Bookworm/Lecons/TypeErasureView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct TypeErasureView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
+    
     var body: some View {
         if sizeClass == .compact {
-            return HStack {
+            return AnyView(VStack {
                 Text("Active size class:")
                 Text("COMPACT")
             }
-            .font(.largeTitle)
+            .font(.largeTitle))
+        } else {
+            return AnyView(HStack {
+                Text("Active size class:")
+                Text("REGULAR")
+            }
+            .font(.largeTitle))
         }
     }
 }
