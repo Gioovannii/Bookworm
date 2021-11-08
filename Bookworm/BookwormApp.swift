b0689c6ac85e4f8996e6fd364901b8fb0156740c
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-08 13:48:51 +0800

    change launch file

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -10,10 +10,10 @@ import SwiftUI
 @main
 struct BookwormApp: App {
     let persistenceController = PersistenceController.shared
-
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TypeErasureView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
